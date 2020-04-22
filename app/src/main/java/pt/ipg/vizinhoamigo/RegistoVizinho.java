cb3d7d7a6465794a272868392cebc9dfde973801
JoaoMoledoDias <40269391+JoaoMoledoDias@users.noreply.github.com>
2020-04-22 21:33:28 +0100

    Criação da Página Layout Lista dos Vizinhos e Criação da Atividade Registo do Novo VIzinho

---
+++ app/src/main/java/pt/ipg/vizinhoamigo/RegistoVizinho.java
@@ -0,0 +1,14 @@
+package pt.ipg.vizinhoamigo;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class RegistoVizinho extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_registo_vizinho);
+    }
+}
