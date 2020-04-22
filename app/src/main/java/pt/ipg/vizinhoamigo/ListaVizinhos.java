0b88dfbc581698d0ff9b6800e2dc97c0b21f976e
JoaoMoledoDias <40269391+JoaoMoledoDias@users.noreply.github.com>
2020-04-22 21:54:36 +0100

    Butões do vizinhos a funcionar

--- app/src/main/java/pt/ipg/vizinhoamigo/ListaVizinhos.java
+++ app/src/main/java/pt/ipg/vizinhoamigo/ListaVizinhos.java
@@ -2,7 +2,9 @@ package pt.ipg.vizinhoamigo;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 public class ListaVizinhos extends AppCompatActivity {
 
@@ -10,5 +12,10 @@ public class ListaVizinhos extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_lista_vizinhos);
+    }
+
+    public void registo_vizinho(View view) {
+        Intent intent = new Intent(this, RegistoVizinho.class);
+        startActivity(intent);
     }
 }
--- app/src/main/java/pt/ipg/vizinhoamigo/MainActivity.java
+++ app/src/main/java/pt/ipg/vizinhoamigo/MainActivity.java
@@ -2,8 +2,10 @@ package pt.ipg.vizinhoamigo;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.graphics.Color;
 import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -13,8 +15,9 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-
-
-
+    }
+    public void lis_vizinhos (View view){
+        Intent intent = new Intent(this, ListaVizinhos.class);
+        startActivity(intent);
     }
 }
